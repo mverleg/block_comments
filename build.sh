00009d54dc6d92781b1817a164b12144aabcfca0
mark <mark@rafiki>
2017-12-09 18:06:12 +0100

    build script

--- build.sh
+++ build.sh
@@ -6,8 +6,14 @@ then
     exit 1
 fi
 
+# Check
+if ! web-ext lint --warnings-as-errors --ignore-files '*.iml' '*.sh'
+then
+    exit 2
+fi
+
 # Build
-web-ext build --overwrite-dest --ignore-files '*.iml'
+web-ext build --overwrite-dest --ignore-files '*.iml' '*.sh'
 
 # Interactive test
 web-ext run
